2f48cb9e13d53543c29be111d2aaa95d589a7bcf
ronak kanthalia <kanthaliar@gmail.com>
2016-12-07 21:41:32 +0530

    updated package.json

--- server/sms.js
+++ server/sms.js
@@ -2,7 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
-	console.log('im here')
-  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
-  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+	Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
+	Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
 }
