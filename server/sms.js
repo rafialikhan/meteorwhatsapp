54bdaad90f56bf03d80b9f16bbcb8cad9a560445
ronak kanthalia <kanthaliar@gmail.com>
2016-12-07 18:58:51 +0530

    meteorwhatsapp

---
+++ server/sms.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
+	console.log('im here')
+  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
+  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+}
